09ab8fba709994a953bbccfc672caf2b29f5f1c6
pamadeu@emeal.nttdata.com <pamadeuc@emeal.nttdata.com>
2025-06-16 16:59:19 -0300

    primeiro commit

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,8 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    it.only('verifica o título da aplicação', () => {
+      cy.visit('./src/index.html')
+    })
+  })
+  
+})
