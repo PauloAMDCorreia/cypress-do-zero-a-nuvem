dee26834b6b974777634b35f48ae7e4d9dcf7ede
pamadeu@emeal.nttdata.com <pamadeuc@emeal.nttdata.com>
2025-07-07 09:54:33 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  projectId: "eajssn",
   e2e: {
     pecPattern: 'cypress/e2e/*.cy.js'
   },
